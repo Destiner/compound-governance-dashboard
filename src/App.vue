4095545a3efd7fdd208fac94250a75c0d04b1e64
Timur Badretdinov <destinerx@gmail.com>
2020-04-22 23:23:23 +0300

    Fix template formatting

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 	<div id="app">
 		<section id="nav">
-			<h1 id="app-title">Compound</h1>
+			<h1 id="app-title">
+				Compound
+			</h1>
 			<div>
 				<div class="nav-section">
 					<div class="nav-links">
